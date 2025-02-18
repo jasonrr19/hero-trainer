973b1ce446a33c57c53ed2bb0c42f71f38ffd3dc
Aditya Kotian <external-services@adityakotian.com>
2025-02-18 15:32:12 +0900

    remove non-index actions

--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -1,26 +1,5 @@
 class LessonsController < ApplicationController
   def index
     @lessons = Lesson.all
-
-    if params[:category]
-      raise
-    end
-  end
-
-  def show
-    @lesson = Lesson.find(params[:id])
-  end
-
-  def new
-    @lesson = Lesson.new
-  end
-
-  def create
-  end
-
-  private
-
-  def lesson_params
-    params.require(:lesson).permit(:title, :description, :duration, :capacity, :price)
   end
 end
