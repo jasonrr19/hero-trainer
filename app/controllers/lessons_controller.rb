b67071f999f017d9c1905cd3e79c4dd5364badcf
Aditya Kotian <external-services@adityakotian.com>
2025-02-18 15:28:23 +0900

    lessons controller and index

--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -1,2 +1,26 @@
 class LessonsController < ApplicationController
+  def index
+    @lessons = Lesson.all
+
+    if params[:category]
+      raise
+    end
+  end
+
+  def show
+    @lesson = Lesson.find(params[:id])
+  end
+
+  def new
+    @lesson = Lesson.new
+  end
+
+  def create
+  end
+
+  private
+
+  def lesson_params
+    params.require(:lesson).permit(:title, :description, :duration, :capacity, :price)
+  end
 end
